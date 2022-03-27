facaf9968c3eda081d0ea357e576a5ae11c05e42
José Lucas <jose.esbellute@gmail.com>
2022-03-26 21:01:59 -0300

    First commit

--- Dant4lion/View/Login.Designer.cs
+++ Dant4lion/View/Login.Designer.cs
@@ -35,8 +35,9 @@ namespace Dant4lion.View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 315);
+            this.ClientSize = new System.Drawing.Size(347, 267);
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
 
