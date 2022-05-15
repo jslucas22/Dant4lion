3634b12eec21ce7167f5ee53f288d7b7c0c9b5dc
José Lucas <jose.esbellute@hotmail.com>
2022-05-15 11:50:48 -0300

    Created ITransmissor interface

---
+++ Dant4lion.Domain/Interfaces/ITransmissor.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace Dant4lion.Domain.Interfaces
+{
+    public interface ITransmissor
+    {
+        /// <summary>
+        /// Envia uma mensagem
+        /// </summary>
+        /// <typeparam name="T">Generic</typeparam>
+        /// <param name="mensagem">Descrição da mensagem</param>
+        void EnviarMensagem<T>(T mensagem) where T : IMessage;
+    }
+}
