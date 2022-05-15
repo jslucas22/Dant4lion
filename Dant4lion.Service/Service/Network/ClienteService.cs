fe7c0bb63505c435778ddc8a334efa81d2120fab
José Lucas <jose.esbellute@hotmail.com>
2022-05-15 11:45:39 -0300

    Created ClienteService

---
+++ Dant4lion.Service/Service/Network/ClienteService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Security;
+
+namespace Dant4lion.Service.Service.Network
+{
+    public class ClienteService
+    {
+        private readonly SslStream _sslStream;
+
+        /// <summary>
+        /// Fecha a conexao entre o cliente e o servidor
+        /// </summary>
+        public void Desconectar()
+        {
+            if (_sslStream != null)
+            {
+                _sslStream.Close();
+            }
+        }
+    }
+}
